95469132d7b218f5bcb2abc18858c8cf2dfd7384
Exosuit <intltcsfleader@gmail.com>
2023-11-15 19:11:27 +0700

    feat: NavBar layout for mobile

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,23 +1,60 @@
-import { Stack, Text, Button, Image, Link } from "@chakra-ui/react";
+import {
+  Stack,
+  Text,
+  Button,
+  Image,
+  Link,
+  Avatar,
+  DrawerBody,
+  Divider,
+  DrawerContent,
+  // DrawerCloseButton,
+  DrawerHeader,
+  DrawerFooter,
+  Drawer,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Stack
       direction={"row"}
       justify={"space-between"}
       align={"center"}
-      bgColor={"blue.200"}
-      px={"8em"}
+      // bgColor={"blue.200"}
+      px={["2em", "2em", "8em", "8em", "8em"]}
       h={"6em"}
     >
-      <Link href="/">
+      <Button
+        variant={"ghost"}
+        color={"white"}
+        display={["flex", "flex", "none", "none", "none"]}
+        onClick={onOpen}
+        _hover={{
+          bgColor: "transparent",
+        }}
+      >
+        <HamburgerIcon w={8} h={8} />
+      </Button>
+
+      <Link href="/" display={["none", "none", "flex", "flex", "flex"]}>
         <Image src="/logo/logo_kitab_navbar.png" />
       </Link>
+
       <Stack
         direction={"row"}
+        display={["none", "none", "flex", "flex", "flex"]}
         align={"center"}
-        w={"36em"}
-        justify={"space-between"}
+        // w={"36em"}
+        w={"full"}
+        justify={"space-evenly"}
+        // mx={"4em"}
       >
         <Link
           href=""
@@ -50,13 +87,17 @@ const NavBar = () => {
           textColor={"white"}
           fontSize={"large"}
         >
-          About Us
+          Tentang Kami
         </Link>
       </Stack>
-      <Stack>
+      {!isLoggedIn && (
         <Button
+          onClick={() => {
+            setIsLoggedIn(!isLoggedIn);
+          }}
           variant={"solid"}
           colorScheme={"blue"}
+          w={"6em"}
           bgGradient="linear(to-r, rgb(1, 16, 141), rgb(1, 152, 189)),"
           _hover={{
             opacity: "1",
@@ -64,7 +105,89 @@ const NavBar = () => {
         >
           Masuk
         </Button>
-      </Stack>
+      )}
+
+      {isLoggedIn && (
+        <Avatar
+          onClick={() => {
+            setIsLoggedIn(!isLoggedIn);
+          }}
+        />
+      )}
+
+      <Drawer isOpen={isOpen} onClose={onClose} placement="left">
+        <DrawerContent
+          outlineColor={"gray.500"}
+          outlineOffset={"-0.5px"}
+          p={"1em"}
+          bgColor={"rgba(56, 68, 100, 0.25)"}
+          backdropFilter={"blur(4px)"}
+          roundedRight={"2em"}
+        >
+          {/* <DrawerCloseButton /> */}
+          <DrawerHeader
+            my={"0.75em"}
+            display={"flex"}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Image src="/logo/logo_kitab_navbar.png" />
+          </DrawerHeader>
+
+          <DrawerBody>
+            <Divider orientation="horizontal" />
+            <Link
+              href=""
+              my={"1em"}
+              opacity={"0.5"}
+              _hover={{
+                opacity: "1",
+              }}
+              textColor={"white"}
+              fontSize={"medium"}
+              display={"flex"}
+              justifyContent={"center"}
+            >
+              Cari Kosan
+            </Link>
+            <Divider orientation="horizontal" />
+            <Link
+              href=""
+              my={"1em"}
+              opacity={"0.5"}
+              _hover={{
+                opacity: "1",
+              }}
+              textColor={"white"}
+              fontSize={"medium"}
+              display={"flex"}
+              justifyContent={"center"}
+            >
+              Kost 101
+            </Link>
+            <Divider orientation="horizontal" />
+            <Link
+              href=""
+              my={"1em"}
+              opacity={"0.5"}
+              _hover={{
+                opacity: "1",
+              }}
+              textColor={"white"}
+              fontSize={"medium"}
+              display={"flex"}
+              justifyContent={"center"}
+            >
+              Tentang Kami
+            </Link>
+            <Divider orientation="horizontal" />
+          </DrawerBody>
+
+          <DrawerFooter>
+            <Text>ini footer ya xixixi</Text>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
     </Stack>
   );
 };
