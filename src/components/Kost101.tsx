02eccd5a799a54d6a6312fab140f022415589e8d
rdhwan <fathanridhwan@gmail.com>
2023-11-17 23:15:45 +0700

    fix: kost101 layouting + make props

--- src/components/Kost101.tsx
+++ src/components/Kost101.tsx
@@ -1,72 +1,110 @@
-import { Stack, Text, Divider, Image } from "@chakra-ui/react";
+import { Stack, Text, Divider, Button } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
-const Kost101 = () => {
+type Article = {
+  id: number;
+  title: string;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+};
+
+const Kost101 = ({
+  articles,
+  isLoadCandidate,
+}: {
+  articles: Article[];
+  isLoadCandidate: boolean;
+}) => {
   return (
     <Stack
       bgColor={"white"}
       borderTopRadius={"2em"}
       boxShadow={"0px 5px 24px 8px rgba(0,0,0,0.3) inset"}
-      paddingStart={"7em"}
-      paddingEnd={"7em"}
-      paddingTop={"1.5em"}
+      px={["4em", "4em", "8em", "8em", "8em"]}
+      py={"2em"}
+      w={"full"}
     >
-      <Stack flexDirection={"row"}>
-        <Text fontSize={"4xl"} as={"b"} flex={"1"} textAlign={"right"}>
-          How to
-        </Text>
+      <Text
+        fontSize={["2xl", "2xll", "4xl", "4xl", "4xl"]}
+        fontWeight={"bold"}
+        flex={"1"}
+        textAlign={"center"}
+      >
+        Artikel{" "}
         <Text
-          fontSize={"4xl"}
-          as={"b"}
-          flex={"1"}
+          fontWeight={"bold"}
+          whiteSpace={"nowrap"}
+          as={"span"}
           bgGradient={"linear(to-t, #0198BD, #01108D)"}
           bgClip="text"
-          paddingBottom={"3vh"}
         >
           Kost101.
         </Text>
+      </Text>
+
+      <Stack>
+        {articles.map((article) => (
+          <>
+            <Divider border={"1px"} borderColor={"#171229"} variant={"solid"} />
+
+            <Stack
+              as={Link}
+              to={`/articles/${article.id}`}
+              flexDirection={"row"}
+              align={"center"}
+              py={"1em"}
+              rounded={"lg"}
+              transition={"all 0.1s ease-in-out"}
+              _hover={{
+                bgColor: "rgba(2, 52, 154, 0.05)",
+              }}
+            >
+              <Stack
+                bgColor={"rgba(2, 52, 154, 0.5)"}
+                h={"fit-content"}
+                rounded={"full"}
+                p={"0.5em"}
+              >
+                <Stack
+                  boxSize={"0.25em"}
+                  rounded={"full"}
+                  outline={"1px solid #02349A"}
+                />
+              </Stack>
+              <Text
+                fontSize={["md", "md", "xl", "xl", "xl"]}
+                ml={"1em"}
+                fontWeight={"semibold"}
+                color={"rgba(0, 0, 0, 0.6)"}
+                // isTruncated
+                noOfLines={2}
+              >
+                {article.title}
+              </Text>
+            </Stack>
+
+            <Divider border={"1px"} borderColor={"#171229"} variant={"solid"} />
+          </>
+        ))}
       </Stack>
 
-      <Stack paddingBottom={"12vh"}>
-        <Divider border={"1px"} borderColor={"#171229"} variant={"solid"} />
-
-        <Stack
-          flexDirection={"row"}
-          paddingBottom={"3.5vh"}
-          paddingTop={"3.5vh"}
+      {isLoadCandidate ? (
+        <Button
+          variant={"ghost"}
+          fontWeight={"semibold"}
+          color={"rgba(0, 0, 0, 0.6)"}
         >
-          <Image src="home/ArticleDotList.png" w={"1.8em"} />
-          {/* isi judul artikel lo dibawah disini cok */}
-          <Text
-            fontSize={"xl"}
-            marginLeft={"1.4em"}
-            fontWeight={"semibold"}
-            color={"rgba(0, 0, 0, 0.6)"}
-          >
-            Surviving "Kost" Life : A Beginner's Guide to Independent Living
-          </Text>
-        </Stack>
-
-        <Divider border={"1px"} borderColor={"#171229"} variant={"solid"} />
-
-        <Stack
-          flexDirection={"row"}
-          paddingBottom={"3.5vh"}
-          paddingTop={"3.5vh"}
+          Load more...
+        </Button>
+      ) : (
+        <Text
+          textAlign={"center"}
+          fontWeight={"semibold"}
+          color={"rgba(0, 0, 0, 0.6)"}
         >
-          <Image src="home/ArticleDotList.png" w={"1.8em"} />
-          {/* isi judul artikel lo dibawah disini cok */}
-          <Text
-            fontSize={"xl"}
-            marginLeft={"1.4em"}
-            fontWeight={"semibold"}
-            color={"rgba(0, 0, 0, 0.6)"}
-          >
-            Budgeting Like a Pro : Financial Hacks for Students
-          </Text>
-        </Stack>
-
-        <Divider border={"1px"} borderColor={"#171229"} variant={"solid"} />
-      </Stack>
+          Thats it...
+        </Text>
+      )}
     </Stack>
   );
 };
