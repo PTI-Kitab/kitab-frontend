d4b0e4fbd776c2b1441619eadfe8e58f98e638a2
Exosuit <intltcsfleader@gmail.com>
2023-11-22 19:21:09 +0700

    fix: image size, button

--- src/components/ListingsList.tsx
+++ src/components/ListingsList.tsx
@@ -5,20 +5,17 @@ const ListingsList = () => {
     <>
       <Stack
         bgColor={"whiteAlpha.200"}
-        flex={"1"}
         borderRadius={"1em"}
         border={"0.05em solid #718096"}
         flexDirection={"row"}
       >
-        <Stack flex={"0.8"}>
-          <Image
-            src="/home/kamar_kos.png"
-            borderRadius={"1em"}
-            w={"100%"}
-            h={"100%"}
-            objectFit={"cover"}
-          />
-        </Stack>
+        <Stack
+          flex={"0.8"}
+          bgImage={"/home/kamar_kos.png"}
+          bgPos={"center"}
+          bgSize={"cover"}
+          rounded={"2xl"}
+        ></Stack>
 
         <Stack flex={"1"}>
           <Stack
@@ -29,52 +26,55 @@ const ListingsList = () => {
             color={"white"}
             w={"100%"}
             h={"100%"}
+            direction={"column"}
+            justify={"space-between"}
           >
-            <Text fontSize={["md", "xl", "2xl", "2xl", "2xl"]}>
-              Treasure Kost
-            </Text>
-            <HStack paddingBottom={["0.2em", "0.8em", "1em", "1em", "1em"]}>
-              <Image src="/logo/LocationIcon.png" w={"0.7em"} />
-              <Text
-                fontSize={["xs", "xs", "sm", "sm", "sm"]}
-                fontWeight={"thin"}
-              >
-                Desa Curug Sangereng, Kelapa Dua
+            <Stack direction={"column"}>
+              <Text fontSize={["md", "xl", "2xl", "2xl", "2xl"]}>
+                Treasure Kost
               </Text>
-            </HStack>
-            <Stack spacing={"0em"}>
-              <Text fontSize={"xs"} fontWeight={"thin"}>
-                Harga mulai dari
-              </Text>
-              <Text
-                fontSize={["xs", "sm", "sm", "sm", "sm"]}
-                fontWeight={"semi"}
-              >
-                Rp. 1.470.000,00/bln
-              </Text>
+              <HStack paddingBottom={["0.2em", "0.8em", "1em", "1em", "1em"]}>
+                <Image src="/logo/LocationIcon.png" w={"0.7em"} />
+                <Text
+                  fontSize={["xs", "xs", "sm", "sm", "sm"]}
+                  fontWeight={"thin"}
+                >
+                  Desa Curug Sangereng, Kelapa Dua
+                </Text>
+              </HStack>
+              <Stack spacing={"0em"}>
+                <Text fontSize={"xs"} fontWeight={"thin"}>
+                  Harga mulai dari
+                </Text>
+                <Text
+                  fontSize={["xs", "sm", "sm", "sm", "sm"]}
+                  fontWeight={"semi"}
+                >
+                  Rp. 1.470.000,00/bln
+                </Text>
+              </Stack>
+              <Stack
+                direction={"column"}
+                display={"flex"}
+                justifyContent={"end"}
+                w={"100%"}
+                paddingTop={["0.5em", "1.5em", "1.8em", "1.8em", "0em"]}
+              ></Stack>
             </Stack>
-            <Stack
-              direction={"column"}
-              display={"flex"}
-              justifyContent={"end"}
-              w={"100%"}
-              paddingTop={["0.5em", "1.5em", "1.8em", "1.8em", "0em"]}
+            <Button
+              w={"10em"}
+              fontSize={"xs"}
+              fontWeight={"semi"}
+              color={"white"}
+              ml={"auto"}
+              borderBottomEndRadius={"1em"}
+              borderBottomStartRadius={"0em"}
+              borderTopStartRadius={"1em"}
+              borderTopEndRadius={"0em"}
+              bgGradient={"linear(to-l, #0198BD, #01108D)"}
             >
-              <Button
-                w={"10em"}
-                fontSize={"xs"}
-                fontWeight={"semi"}
-                color={"white"}
-                ml={"auto"}
-                borderBottomEndRadius={"1em"}
-                borderBottomStartRadius={"0em"}
-                borderTopStartRadius={"1em"}
-                borderTopEndRadius={"0em"}
-                bgGradient={"linear(to-l, #0198BD, #01108D)"}
-              >
-                Booking Sekarang!
-              </Button>
-            </Stack>
+              Booking Sekarang!
+            </Button>
           </Stack>
         </Stack>
       </Stack>
