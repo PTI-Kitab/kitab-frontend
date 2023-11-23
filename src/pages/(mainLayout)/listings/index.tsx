37106b866121532630efe6bb5a2618f5c022f41b
rdhwan <fathanridhwan@gmail.com>
2023-11-24 00:11:56 +0700

    feat: listing page integrations

--- src/pages/(mainLayout)/listings/index.tsx
+++ src/pages/(mainLayout)/listings/index.tsx
@@ -7,37 +7,123 @@ import {
   InputGroup,
   InputRightElement,
   Show,
-  Hide,
   Button,
   Popover,
   PopoverTrigger,
   PopoverContent,
-  PopoverHeader,
   PopoverBody,
-  PopoverFooter,
-  PopoverArrow,
   PopoverCloseButton,
-  PopoverAnchor,
+  FormLabel,
+  RangeSlider,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  RangeSliderTrack,
+  Select,
+  Spinner,
+  Tooltip,
 } from "@chakra-ui/react";
-import { SearchIcon } from "@chakra-ui/icons";
+import {
+  ChevronDownIcon,
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  SearchIcon,
+} from "@chakra-ui/icons";
 import ListingsList from "@/components/ListingsList";
+import useApi, { ResponseModel, useToastErrorHandler } from "@/hooks/useApi";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/useHook";
+import { useForm } from "react-hook-form";
+
+type Kamar = {
+  GambarKamar: string[];
+  id: number;
+  gender: "pria" | "wanita" | "campur";
+  kost: {
+    id: number;
+    namaKost: string;
+    alamat: string;
+  };
+  namaKamar: string;
+  harga: number;
+  ukuran: string;
+  capacity: number;
+};
+
+type ListingFilter = {
+  hargaGte: number;
+  hargaLte: number;
+  gender?: "pria" | "wanita" | "campur";
+  search?: string;
+};
 
 const ListingPage = () => {
+  const api = useApi();
+  const handleError = useToastErrorHandler();
+
+  const { register, handleSubmit } = useForm<{ search: string }>();
+
+  const [listings, setListings] = useState<Kamar[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const [filter, setFilter] = useState<ListingFilter>({
+    hargaGte: 0,
+    hargaLte: 10000000,
+  });
+  const [page, setPage] = useState<number>(1);
+
+  const debouncedFilter = useDebounce(filter, 500);
+
+  const fetchListing = () => {
+    setLoading(true);
+
+    const urlBuilder = new URLSearchParams();
+    urlBuilder.append("hargaGte", debouncedFilter.hargaGte.toString());
+    urlBuilder.append("hargaLte", debouncedFilter.hargaLte.toString());
+    urlBuilder.append("page", page.toString());
+    if (debouncedFilter.search)
+      urlBuilder.append("search", debouncedFilter.search);
+    if (debouncedFilter.gender)
+      urlBuilder.append("gender", debouncedFilter.gender);
+
+    api
+      .get<ResponseModel<Kamar[]>>(`/listings?${urlBuilder.toString()}`)
+      .then((res) => {
+        setListings(res.data.data);
+      })
+      .catch(handleError)
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(() => {
+    fetchListing();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [debouncedFilter]);
+
   return (
     <Stack
       px={["2em", "2em", "8em", "8em", "8em"]}
       py={["0em", "0em", "1em", "2em", "2em"]}
     >
-      <Stack paddingBottom={"3em"}>
-        <Box>
-          <Image src="/background/blackLives.png" w={"90em"} />
-        </Box>
-      </Stack>
+      <Box>
+        <Image src="/background/blackLives.png" w={"90em"} />
+      </Box>
 
-      <Stack paddingBottom={"3em"}>
-        <Box>
-          <InputGroup w={"100%"} bgColor={"whiteAlpha.200"} rounded={"1em"}>
+      <Stack my={"1em"}>
+        <form
+          onSubmit={handleSubmit((data) => {
+            setFilter((filter) => ({ ...filter, search: data.search }));
+          })}
+        >
+          <InputGroup
+            w={"100%"}
+            bgColor={"whiteAlpha.200"}
+            backdropFilter={"blur(4px)"}
+            rounded={"1em"}
+            color={"white"}
+          >
             <Input
+              {...register("search")}
               rounded={"1em"}
               borderColor={"gray.500"}
               placeholder="Treasure Kost, Warteg Murah, Kost terdekat dari UMN"
@@ -47,30 +133,90 @@ const ListingPage = () => {
               <SearchIcon pr={"1em"} boxSize={"10"} color={"gray.500"} />
             </InputRightElement>
           </InputGroup>
-        </Box>
+        </form>
       </Stack>
 
       {/* mobile filter panel (button) */}
 
       <Show breakpoint="(max-width: 1020px)">
-        <Stack display={"flex"} alignItems={"end"}>
+        <Stack alignItems={"end"}>
           <Popover placement="bottom-end">
             <PopoverTrigger>
               <Button
-                fontSize={"xl"}
-                bgGradient={"linear(to-l, #0198BD, #01108D)"}
                 rounded={"0.5em"}
                 color={"white"}
-                w={"10em"}
+                variant={"unstyled"}
+                rightIcon={<ChevronDownIcon />}
               >
                 Filter
               </Button>
             </PopoverTrigger>
-            <PopoverContent>
+            <PopoverContent
+              rounded={"2xl"}
+              bgColor={"blackAlpha.700"}
+              backdropFilter={"blur(4px)"}
+              p={"1em"}
+            >
               <PopoverCloseButton />
-              <PopoverHeader>Confirmation!</PopoverHeader>
               <PopoverBody>
-                Are you sure you want to have that milkshake?
+                <Stack my={"1em"} color={"white"}>
+                  <FormLabel>Harga</FormLabel>
+                  <RangeSlider
+                    aria-label={["min", "max"]}
+                    defaultValue={[100000, 10000000]}
+                    min={100000}
+                    max={10000000}
+                    step={100000}
+                    onChangeEnd={(val) => {
+                      setFilter({
+                        ...filter,
+                        hargaGte: val[0],
+                        hargaLte: val[1],
+                      });
+                    }}
+                  >
+                    <RangeSliderTrack>
+                      <RangeSliderFilledTrack />
+                    </RangeSliderTrack>
+                    <Tooltip
+                      label={filter.hargaGte.toLocaleString("id-ID", {
+                        style: "currency",
+                        currency: "IDR",
+                      })}
+                    >
+                      <RangeSliderThumb index={0} />
+                    </Tooltip>
+                    <Tooltip
+                      label={filter.hargaLte.toLocaleString("id-ID", {
+                        style: "currency",
+                        currency: "IDR",
+                      })}
+                    >
+                      <RangeSliderThumb index={1} />
+                    </Tooltip>
+                  </RangeSlider>
+                  <FormLabel>Gender</FormLabel>
+                  <Select
+                    color={"white"}
+                    placeholder="Pilih Gender"
+                    rounded={"1em"}
+                    borderColor={"gray.500"}
+                    bgColor={"whiteAlpha.200"}
+                    onChange={(val) => {
+                      setFilter((filter) => ({
+                        ...filter,
+                        gender: val.target.value as
+                          | "pria"
+                          | "wanita"
+                          | "campur",
+                      }));
+                    }}
+                  >
+                    <option value="pria">Pria</option>
+                    <option value="wanita">Wanita</option>
+                    <option value="campur">Campur</option>
+                  </Select>
+                </Stack>
               </PopoverBody>
             </PopoverContent>
           </Popover>
@@ -83,20 +229,130 @@ const ListingPage = () => {
         <Show breakpoint="(min-width: 1020px)">
           <Stack
             bgColor={"whiteAlpha.200"}
+            backdropFilter={"blur(4px)"}
             flex={"1"}
             borderRadius={"1em"}
             border={"0.05em solid #718096"}
             padding={"1em"}
+            color={"white"}
+            h={"fit-content"}
           >
-            <Text fontWeight={600}>Put your filter here</Text>
+            <Text fontSize={["md", "xl", "2xl", "2xl", "2xl"]}>Filter</Text>
+
+            <Stack my={"1em"}>
+              <FormLabel>Harga</FormLabel>
+              <RangeSlider
+                aria-label={["min", "max"]}
+                defaultValue={[100000, 10000000]}
+                min={100000}
+                max={10000000}
+                step={100000}
+                onChangeEnd={(val) => {
+                  setFilter({
+                    ...filter,
+                    hargaGte: val[0],
+                    hargaLte: val[1],
+                  });
+                }}
+              >
+                <RangeSliderTrack>
+                  <RangeSliderFilledTrack />
+                </RangeSliderTrack>
+                <Tooltip
+                  label={filter.hargaGte.toLocaleString("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                  })}
+                >
+                  <RangeSliderThumb index={0} />
+                </Tooltip>
+                <Tooltip
+                  label={filter.hargaLte.toLocaleString("id-ID", {
+                    style: "currency",
+                    currency: "IDR",
+                  })}
+                >
+                  <RangeSliderThumb index={1} />
+                </Tooltip>
+              </RangeSlider>
+              <FormLabel>Gender</FormLabel>
+              <Select
+                color={"white"}
+                placeholder="Pilih Gender"
+                rounded={"1em"}
+                borderColor={"gray.500"}
+                bgColor={"whiteAlpha.200"}
+                onChange={(val) => {
+                  setFilter((filter) => ({
+                    ...filter,
+                    gender: val.target.value as "pria" | "wanita" | "campur",
+                  }));
+                }}
+              >
+                <option value="pria">Pria</option>
+                <option value="wanita">Wanita</option>
+                <option value="campur">Campur</option>
+              </Select>
+            </Stack>
           </Stack>
         </Show>
         <Stack
           flex={"2"}
-          borderRadius={"1em"}
-          paddingStart={["0em", "0em", "0em", "1em", "1em"]}
+          // borderRadius={"1em"}
+          ml={["0em", "0em", "0em", "1em", "1em"]}
+          overflow={"scroll"}
+          spacing={["1em", "1em", "1em", "0em", "0em"]}
+          gap={"1em"}
         >
-          <ListingsList />
+          {loading && (
+            <Stack flex={1} align={"center"} justify={"center"}>
+              <Spinner size={"lg"} color="white" />
+              <Text color={"white"}>Loading...</Text>
+            </Stack>
+          )}
+
+          {!loading && listings.length === 0 && (
+            <Stack flex={1} align={"center"} justify={"center"}>
+              <Text fontSize={"xl"} color={"white"}>
+                Tidak ada listing yang ditemukan
+              </Text>
+            </Stack>
+          )}
+          {listings.map((listing) => (
+            <ListingsList kamar={listing} />
+          ))}
+        </Stack>
+      </Stack>
+      <Stack w={"full"} align={"center"} justify={"center"} my={"1em"}>
+        <Stack direction={"row"}>
+          <Button
+            rounded={"full"}
+            bgColor={"whiteAlpha.200"}
+            color={"white"}
+            onClick={() => setPage(page - 1)}
+            isDisabled={page === 1}
+          >
+            <ChevronLeftIcon />
+          </Button>
+          <Input
+            w={"3em"}
+            rounded={"2xl"}
+            // defaultValue ={page}
+            color={"white"}
+            value={page}
+            onChange={(e) => setPage(Number(e.target.value))}
+            textAlign={"center"}
+            type="number"
+          />
+          <Button
+            rounded={"full"}
+            bgColor={"whiteAlpha.200"}
+            color={"white"}
+            onClick={() => setPage(page + 1)}
+            isDisabled={listings.length < 5}
+          >
+            <ChevronRightIcon />
+          </Button>
         </Stack>
       </Stack>
     </Stack>
