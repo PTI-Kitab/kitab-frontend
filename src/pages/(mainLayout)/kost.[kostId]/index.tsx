d6fe93933e3b8e251c3318dc293c004c52a17f21
rdhwan <fathanridhwan@gmail.com>
2023-11-24 00:12:08 +0700

    feat: kost detail page integration

--- src/pages/(mainLayout)/kost.[kostId]/index.tsx
+++ src/pages/(mainLayout)/kost.[kostId]/index.tsx
@@ -1,9 +1,103 @@
-import { MacWindowModel } from "@/components/WindowModel";
-import { Icon, Stack, Text, Divider, AspectRatio } from "@chakra-ui/react";
+import {
+  Icon,
+  Stack,
+  Text,
+  AspectRatio,
+  Spinner,
+  Image,
+  Button,
+  Input,
+} from "@chakra-ui/react";
 import { FaLocationDot } from "react-icons/fa6";
 import ListingsList from "@/components/ListingsList";
+import useApi, { ResponseModel, useToastErrorHandler } from "@/hooks/useApi";
+import { useEffect, useState } from "react";
+import { useParams } from "@/router";
+import { ChevronLeftIcon, ChevronRightIcon } from "@chakra-ui/icons";
+
+type KostDetail = {
+  id: number;
+  namaKost: string;
+  description: string;
+  alamat: string;
+  latitude: string;
+  longitude: string;
+  pembayaran: string;
+  bank: string;
+  pemilik: Pemilik;
+  pemilikId: number;
+  createdAt: Date;
+  updatedAt: Date;
+  GambarKost: string[];
+  Kamar: Kamar[];
+};
+
+type Pemilik = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  noHp: string;
+};
+
+type Kamar = {
+  id: number;
+  namaKamar: string;
+  description: string;
+  harga: number;
+  fasilitas: string;
+  ukuran: string;
+  gender: "pria" | "wanita" | "campur";
+  capacity: number;
+  kost: {
+    id: number;
+    namaKost: string;
+    alamat: string;
+  };
+  kostId: number;
+  createdAt: Date;
+  updatedAt: Date;
+  GambarKamar: string[];
+};
 
 const KostPage = () => {
+  const api = useApi();
+  const errorHandler = useToastErrorHandler();
+  const params = useParams("/kost/:kostId");
+
+  const [kost, setKost] = useState<KostDetail | null>(null);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    api
+      .get<ResponseModel<KostDetail>>(`/client/kost/${params.kostId}`)
+      .then((res) => {
+        setKost(res.data.data);
+      })
+      .catch(errorHandler)
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [params]);
+
+  if (isLoading) {
+    return (
+      <Stack flex={1} align={"center"} justify={"center"}>
+        <Spinner size={"xl"} color="white" />
+      </Stack>
+    );
+  }
+
+  if (!kost) {
+    return (
+      <Stack flex={1} align={"center"} justify={"center"}>
+        <Text>Tidak dapat menemukan kost</Text>
+      </Stack>
+    );
+  }
+
   return (
     <Stack>
       <Stack
@@ -19,88 +113,55 @@ const KostPage = () => {
         p={"1em"}
         px={["1em", "1em", "4em", "4em", "4em"]}
       >
-        <Text fontSize={"xxx-large"}>Treasure Kost</Text>
+        <Text fontSize={"xxx-large"}>{kost.namaKost}</Text>
         <Stack direction={"row"} align={"center"}>
           <Icon as={FaLocationDot} />
-          <Text>Desa Curug Sangereng, Kelapa Dua</Text>
+          <Text>{kost.alamat}</Text>
         </Stack>
 
-        <Text fontWeight={"light"} pt={"2em"}>
-          Kosan di daerah desa curug sangean, ada 4 lantai di jaga sama pace.
-          Yang punya haji kaki pincang satu naik mio putih. Kosan ini memiliki
-          fasilitas mesin cuci untuk setiap lantai tetapi mesin cuci lantai 3
-          sering kebalik karena overloading, mohon untuk mengisi pakaian dengan
-          benar karena mesin cuci lantai 2 disassembly sampai harus beli baru.
-        </Text>
-        <Text fontWeight={"light"} pt={"1em"}>
-          Selain itu, kosan ini memiliki dua tempat parkir yaitu tempat parkir
-          dalam dan luar, tetapi dimohon untuk tidak parkir motor vixion biru di
-          parkiran depan karena akan dimarahin oleh pak Herman (orang Flores).
-          Kosan ini memiliki fasilitas wifi yang bisa diakses dengan memohon-
-          mohon kode vocer kepada pa dul.
-        </Text>
-        <Text fontWeight={"light"} pt={"1em"}>
-          Kosan ini memiliki fasilitas dapur bersama untuk setiap lantai tetapi
-          harap pengertian untuk menggunakan dan mengambil bahan makanan dari
-          kulkas karena terjadi sebuah insiden dimana seorang penghuni membeli
-          sekantong nugget dan dihabiskan oleh orang tidak dikenal sampai hanya
-          tersisa 2 biji. Penghuni tresur kos dimohon untuk tidak mencolok les
-          kuker di luar karena akan menyebabkan mati lampu pada gedung.
-        </Text>
-        <Divider orientation="horizontal" pt={"2em"} />
-        <Text fontSize={"xx-large"} pt={"1em"}>
-          Kost dikelola oleh:
-        </Text>
-        <Text fontSize={"large"}>Pak Haji Dul (PHD)</Text>
-        <Text fontSize={"large"}>No Telp: </Text>
-        <Divider orientation="horizontal" pt={"2em"} gap={0} />
-        <Stack>
-          <Stack direction={"row"} justify={"space-evenly"}>
-            <Stack direction={"column"}>
-              <Text fontSize={"x-large"} pt={"1em"}>
-                Detail Kamar
-              </Text>
-              <Stack direction={"column"} px={"1em"}>
-                <Text>Tipe kamar 3x3</Text>
-                <Text>Kamar mandi dalam</Text>
-                <Text>Listrik (tidak termasuk)</Text>
-                <Text>Air (termasuk)</Text>
-              </Stack>
-            </Stack>
-
-            <Stack direction={"column"}>
-              <Text fontSize={"x-large"} pt={"1em"}>
-                Fitur Tambahan
-              </Text>
-              <Stack direction={"column"} px={"1em"}>
-                <Text>Wi-Fi</Text>
-                <Text>Mesin cuci</Text>
-                <Text>Jemuran</Text>
-                <Text>Pendingin ruangan</Text>
-              </Stack>
-            </Stack>
-          </Stack>
-          <Divider orientation="horizontal" pt={"2em"} gap={0} />
-          <Stack direction={"row"} justify={"start"}>
-            <Stack direction={"column"}>
-              <Text fontSize={"xx-large"}>Peraturan Kamar</Text>
-              <Stack direction={"column"} px={"1em"}>
-                <Text fontSize={"large"}>
-                  Tidak diperbolehkan menginap (kalo tititnya kecil)
-                </Text>
-                <Text fontSize={"large"}>
-                  Tidak diperbolehkan membawa hewan peliharaan
-                </Text>
-                <Text fontSize={"large"}>
-                  Batas tamu berkunjung smapai dengan jam 10 malam
-                </Text>
-              </Stack>
-            </Stack>
-          </Stack>
+        <Stack
+          direction={"row"}
+          justify={"center"}
+          flexWrap={"wrap"}
+          gap={"1em"}
+          my={"1em"}
+        >
+          {kost?.GambarKost.map((gambar, index) => (
+            <Image
+              key={index}
+              src={gambar}
+              alt={"Gambar kost"}
+              w={["full", "full", "6em", "12em", "14em"]}
+              h={"10em"}
+              objectFit={"cover"}
+              rounded={"xl"}
+            />
+          ))}
         </Stack>
+
+        <Stack fontWeight={"light"} my={"1em"}>
+          <div
+            dangerouslySetInnerHTML={{
+              __html: kost.description,
+            }}
+          ></div>
+        </Stack>
+
+        <Stack my={"1em"} gap={0}>
+          <Text fontSize={"2xl"}>Dikelola Oleh</Text>
+          <Text>
+            {kost.pemilik.firstName} {kost.pemilik.lastName}
+          </Text>
+          <Text>
+            {kost.pemilik.email} - {kost.pemilik.noHp}
+          </Text>
+        </Stack>
+
         <Stack pt={"2em"} rounded={"2em"}>
           <AspectRatio ratio={[16 / 10, 16 / 10, 16 / 10, 16 / 5, 16 / 5]}>
-            <iframe src="https://maps.google.com/maps?q=-6.253803,106.617185&hl=en&z=14&amp;output=embed"></iframe>
+            <iframe
+              src={`https://maps.google.com/maps?q=${kost.latitude},${kost.longitude}&hl=en&z=14&amp;output=embed`}
+            ></iframe>
           </AspectRatio>
         </Stack>
       </Stack>
@@ -115,8 +176,52 @@ const KostPage = () => {
         textShadow={"md"}
         p={"1em"}
         px={["1em", "1em", "4em", "4em", "4em"]}
+        gap={"1em"}
       >
-        <ListingsList />
+        {kost.Kamar.length === 0 && (
+          <Text textAlign={"center"}>Tidak ada kamar yang tersedia</Text>
+        )}
+
+        {
+          // paginate kamar by 5 items per page
+          kost.Kamar.slice((page - 1) * 5, page * 5).map((kamar, index) => (
+            <ListingsList key={index} kamar={kamar} />
+          ))
+        }
+
+        <Stack w={"full"} align={"center"} justify={"center"} my={"1em"}>
+          <Stack direction={"row"}>
+            <Button
+              rounded={"full"}
+              bgColor={"whiteAlpha.200"}
+              color={"white"}
+              onClick={() => setPage(page - 1)}
+              isDisabled={page === 1}
+            >
+              <ChevronLeftIcon />
+            </Button>
+            <Input
+              w={"3em"}
+              rounded={"2xl"}
+              // defaultValue ={page}
+              color={"white"}
+              value={page}
+              onChange={(e) => setPage(Number(e.target.value))}
+              textAlign={"center"}
+              type="number"
+            />
+            <Button
+              rounded={"full"}
+              bgColor={"whiteAlpha.200"}
+              color={"white"}
+              onClick={() => setPage(page + 1)}
+              // isDisabled={listings.length < 5}
+              isDisabled={kost.Kamar.length < page * 5}
+            >
+              <ChevronRightIcon />
+            </Button>
+          </Stack>
+        </Stack>
       </Stack>
     </Stack>
   );
